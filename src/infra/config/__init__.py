0928bcd2a7e2ef7ad7773f0946cd196781fab3dc
Vanessa <vanessa.nunez.p@gmail.com>
2022-06-18 20:23:16 -0300

    feat: implementing infra with config, entities and repo

---
+++ src/infra/config/__init__.py
@@ -0,0 +1,9 @@
+from .db_base import Base
+from .db_config import DBConnectionHandler
+
+"""
+    Declaramos que apenas poderá ser usada para fora desse módulo o Base (de db_base)
+    e DBConnectionHandle (de db_config)
+"""
+
+""" barramento no init """
